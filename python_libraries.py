8de9bdaa3012690d310c377bac9b2a4999b6f92b
bcandelon <b.candelon@gmail.com>
2021-11-11 09:44:32 +0100

    Clean up

--- python_libraries.py
+++ python_libraries.py
@@ -1,17 +1,18 @@
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 
 da = pd.read_csv("Nhanes.csv")
 
 # Print out the first few rows of the data
-# print(da.BPXSY1.head())
+print(da.head())
+
 # print(da.DMDEDUC2.value_counts())
-da["DMDEDUC2x"] = da.DMDEDUC2x.fillna("Missing")
+da["DMDEDUC2x"] = da.DMDEDUC2.fillna("Missing")
 # print(da.DMDEDUC2x.value_counts())
 da["RIAGENDRx"] = da.RIAGENDR.replace({1: "Male", 2: "Female"})
-x = da.DMDEDUC2x.value_counts()  # x is just a name to hold this value temporarily
+# x = da.DMDEDUC2x.value_counts()  # x is just a name to hold this value temporarily
 
 # print(x.median())
 # print(x.quantile(0.5))
