b4f412d7b7de8272b6c075bcdc99aa492adf4639
bcandelon <b.candelon@gmail.com>
2021-11-19 16:39:18 +0100

    Multivariate Data Selection

--- multivariate-data.py
+++ multivariate-data.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 import pandas as pd
 pd.set_option('display.max_columns', 100)
 df = pd.read_csv("./Nhanes.csv")
@@ -8,7 +8,14 @@ df = pd.read_csv("./Nhanes.csv")
 keep = [column for column in df.columns if 'BM' in column]
 # print(df.loc[:,keep])
 # print(df[keep])
-index_bool = np.isin(df.columns, keep)
+# index_bool = np.isin(df.columns, keep)
 # print(index_bool)
-print(df.iloc[:, index_bool])
+# print(df.iloc[:, index_bool])
+# Waist larger than median
+waist_median = pd.Series.median(df['BMXWAIST'])
+# print(waist_median)
+# print([df['BMXWAIST'] > waist_median ])
+condition1 = df['BMXWAIST'] > waist_median
+condition2 = df['BMXLEG'] < 32
+print(df.loc[condition1 & condition2, :])
 print("Finished")
